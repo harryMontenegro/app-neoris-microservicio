d37b142dbf6dbeb1a777fc9f80d8c884c0242fcb
harrymontenegro <harryjm018@hotmail.com>
2023-01-10 15:36:51 -0500

    Proyecto microservicios neoris app

--- servicio-personas/src/test/java/com/neoris/app/serviciopersonas/testjpa/PersonaRepositoryTest.java
+++ servicio-personas/src/test/java/com/neoris/app/serviciopersonas/testjpa/PersonaRepositoryTest.java
@@ -1,4 +1,5 @@
 package com.neoris.app.serviciopersonas.testjpa;
+
 import com.neoris.app.serviciopersonas.repository.GeneroRepository;
 import com.neoris.app.serviciopersonas.repository.PersonaRepository;
 import org.junit.jupiter.api.Test;
@@ -20,29 +21,4 @@ public class PersonaRepositoryTest {
         this.personaRepository = personaRepository;
         this.generoRepository = generoRepository;
     }
-
-    @Transactional
-    @Test
-    void validarGuardado(){
-/*        Genero genero = Genero.builder()
-                .codigo("M")
-                .nombre("Masculino")
-                .estado(true)
-                .build();
-        genero = generoRepository.save(genero);
-
-        Persona persona = Persona.builder()
-                .identificacion("109812345")
-                .nombres("Jose Lema")
-                .idGenero(genero.getIdGenero())
-                .edad(25)
-                .direccion("Otavalo sn y principal")
-                .telefono("098254785")
-                .estado(true).build();
-        personaRepository.save(persona);
-
-        persona = personaRepository.getById(1L);
-        Assertions.assertEquals(1, persona.getIdPersona());*/
-    }
-
 }
